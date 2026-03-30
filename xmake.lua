8d0a65e52456cbfe39bcfe90077f9388321f8323
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-03-30 12:25:10 +0100

    add tasks to xmake

--- xmake.lua
+++ xmake.lua
@@ -8,10 +8,33 @@ add_rules("mode.debug", "mode.release")
 add_requires("cli11 2.6.2")
 add_requires("raylib master") -- develop against this until 6.0 is released
 add_requires("quickjs-ng 0.13.0")
-add_requires("spdlog 1.16")
+
+task("bind-raylib")
+    on_run(function ()
+        local script_dir = path.join(os.projectdir(), "tools/autobind")
+        local script_path = "bind.py"
+        os.execv("python", {script_path}, {curdir = script_dir})
+        print("Raylib bindings generated successfully from: %s", script_dir)
+    end)
+    set_menu {
+        usage = "xmake bind-raylib",
+        description = "Generate C++ bindings for Raylib using the python script"
+    }
+
+task("compile-runtime")
+    on_run(function ()
+        os.execv("qjsc", {"-N", "qjsc_runtime", "-C", "-m", "-o", "src/js/runtime.h", "src/js/runtime.js"})
+        print("JavaScript runtime compiled to src/js/runtime.h")
+    end)
+    set_menu {
+        usage = "xmake compile-runtime",
+        description = "Compile runtime.js to a C header using qjsc"
+    }
 
 target("arge")
     set_kind("binary")
     add_files("src/*.cpp")
     add_files("src/raylib/*.cpp")
     add_packages("cli11", "raylib", "quickjs-ng")
+
+
